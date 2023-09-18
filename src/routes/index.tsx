c39bb41d9a346cae5cc454270218e5ddd8ae81f6
Christiano-K-Oliveira <kayky.20oliveira@gmail.com>
2023-09-18 18:41:07 -0300

    feat: feito todas as configurações iniciais

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+
+const RoutesMain = () => {
+    <Routes>
+        <Route path='/' element={''}/>
+        <Route path='/planos' element={''}/>
+        <Route path='/sobre' element={''}/>
+    </Routes>
+}
+
+export default RoutesMain
